5ef7611cfb4ae8fac2ed3ff45d5bedc8f1b8093c
Suhasmashetti <suhasmashety@gmail.com>
2025-06-04 18:01:35 +0530

    updated userefs

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -3,36 +3,31 @@ import { footerLinks } from '../constants'
 
 const Footer = () => {
   return (
-    <footer className="py-5 sm:px-10 px-5">
+    <footer className="py-5 sm:px-10 px-5 bg-black">
       <div className="screen-max-width">
         <div>
-          <p className="font-semibold text-gray text-xs">
-            More ways to shop: {' '}
-            <span className="underline text-blue">
-            Find an Apple Store {' '}
-            </span>
-            or {' '}
-            <span className="underline text-blue">
-            other retailer
-            </span>{' '}
-            near you.
+          <p className="font-semibold text-gray-400 text-xs">
+            More ways to shop:{' '}
+            <span className="underline text-blue-400 hover:cursor-pointer hover:text-blue">Find an Apple Store</span> or{' '}
+            <span className="underline text-blue-400 hover:cursor-pointer hover:text-blue">other retailer</span> near you.
           </p>
-          <p className="font-semibold text-gray text-xs">
+          <p className="font-semibold text-gray-400 text-xs">
             Or call 000800-040-1966
           </p>
+          <div className="text-gray-500 text-xs mt-2">Made by Suhas 🫶</div>
         </div>
 
         <div className="bg-neutral-700 my-5 h-[1px] w-full" />
 
-        <div className="flex md:flex-row flex-col md:items-center justify-between">
-          <p className="font-semibold text-gray text-xs">Copright @ 2024 Apple Inc. All rights reserved.</p>
-          <div className="flex">
+        <div className="flex md:flex-row flex-col md:items-center justify-between gap-2">
+          <p className="font-semibold text-gray-400 text-xs">
+            © 2024 Apple Inc. All rights reserved.
+          </p>
+          <div className="flex flex-wrap hover:cursor-pointer">
             {footerLinks.map((link, i) => (
-              <p key={link} className="font-semibold text-gray text-xs">
-                {link}{' '}
-                {i !== footerLinks.length - 1 && (
-                  <span className="mx-2"> | </span>
-                )}
+              <p key={link} className="font-semibold text-gray-400 text-xs hover:text-blue">
+                {link}
+                {i !== footerLinks.length - 1 && <span className="mx-2">|</span>}
               </p>
             ))}
           </div>
